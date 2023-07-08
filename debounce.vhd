0638cd6d08ee50ec01dc204710a33068f5b875d3
lucas <91328518+llucasroot@users.noreply.github.com>
2023-07-08 18:13:52 -0300

    Update debounce.vhd

--- debounce.vhd
+++ debounce.vhd
@@ -1,25 +1,17 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
-
-
 entity debounce is
 generic (max : integer := 10e6); -- 1 milh?o = 10ms em clock de 100Mhz
     Port ( clk : in  STD_LOGIC;
            botao : in  STD_LOGIC;
            result : out  STD_LOGIC);
 end debounce;
-
 architecture Behavioral of debounce is
-
 signal botao_ant : std_logic  := '0'; 
-
 begin
 	process(clk)
 	variable cont : integer range 0 to max := 0;
 	begin
-	
 		if (clk'event and clk='1')
 				then	
 				      botao_ant <= botao ; -- salva o estado anterior do botao
@@ -31,14 +23,11 @@ begin
 						
 						if botao='0' and botao_ant = '1' then cont:=1;
 						end if;
-						
                   if cont > 0 then
 						         if cont < max then cont := cont +1;
 							                    else cont := 0;
 							      end if;
 						end if;
-		  
 		end if;  -- clk'event
 	end process;
-
 end Behavioral;
